b2c81845376183afa734610facce1f78c4db59fb
Cyan <cyan.lin@wpgholdings.com>
2025-04-07 15:15:27 +0800

    first commit

--- src/app/[location]/[q]/page.tsx
+++ src/app/[location]/[q]/page.tsx
@@ -1,20 +1,30 @@
+// PageProps 是什麼?
+// PageProps 是 Next.js 的頁面屬性
+
 import Header from "@/components/Header";
 import RestaurantItem from "@/components/RestaurantItem";
 import { getAllTags, locations, searchRestaurants } from "@/data/restaurants";
 import { Metadata } from "next";
 import { cache } from "react";
 
+// 它定義了頁面的參數
 interface PageProps {
   params: Promise<{ location: string; q: string }>;
 }
 
-export const revalidate = 86400; // Refresh cached pages once every 24 hours
+// 設定頁面重新渲染的時間，單位是秒，一旦偵測到這個快取過期了，會在背景(server)重新渲染，但是該user會拿到cache的那份(避免render太久)
+// 直到背景重新渲染完畢了，這時候重新整理頁面，就會拿到最新的資料
+export const revalidate = 24 * 60 * 60; // 24小時
 
 export async function generateStaticParams() {
-  const allTags = await getAllTags({
-    // If you have very many pages, you can only render a subset at compile-time. The rest will be rendered & cached at first access.
-    // limit: 10
-  });
+  // 如果回傳空陣列，表示首次進入所有頁面的時候會渲染一次，之後就會暫存起來，其他user進入頁面時都不會重新渲染(ISR)
+  // return [];
+
+  // 如果回傳所有可能的參數，表示這些頁面會在build時就生成，數量多的話，可能會生成比較久
+  // 所以我們可以限制要在build時就生成的數量，減少build時間
+  const allTags = await getAllTags();
+  // 如果有太多的page要SSG，會讓build時間變得很久，這時候可以設定只有特定的熱門params才採用SSG，其他的用ISR(第一次需要渲染，後續都用cache)
+  // { limit: 10 }
 
   return allTags
     .map((tag) =>
@@ -26,12 +36,16 @@ export async function generateStaticParams() {
     .flat();
 }
 
+// 因為我們不能在generateMetadata 和 Page 共享資料，需要 fetch 兩次
+// 所以都會使用 cache 來暫存資料，這樣只會到資料庫 fetch 一次
+// 就不會有非必要的資料庫 fetch
 const getRestaurants = cache(searchRestaurants);
 
+// generateMetadata 可以讓我們在頁面中生成metadata
 export async function generateMetadata({
   params,
 }: PageProps): Promise<Metadata> {
-  const { q, location } = await params;
+  const { location, q } = await params;
 
   const qDecoded = decodeURIComponent(q);
   const locationDecoded = decodeURIComponent(location);
@@ -44,9 +58,16 @@ export async function generateMetadata({
   };
 }
 
+// 為什麼需要async?
+// 因為我們需要從資料庫中取得資料
+// 資料庫的資料是動態的，所以需要使用async
+// 如果我們使用普通的function，我們會得到一個錯誤
+// 因為我們不能在普通的function中使用await
 export default async function Page({ params }: PageProps) {
-  const { q, location } = await params;
+  const { location, q } = await params;
 
+  // 解碼: 因為我們的url是編碼過的，所以需要解碼，再傳給searchRestaurants
+  // 例如: %E9%A3%AF%E5%BA%97 解碼後變成 餐廳
   const qDecoded = decodeURIComponent(q);
   const locationDecoded = decodeURIComponent(location);
 
@@ -55,8 +76,8 @@ export default async function Page({ params }: PageProps) {
   return (
     <div>
       <Header q={qDecoded} location={locationDecoded} />
-      <main className="container mx-auto space-y-8 px-4 py-8">
-        <h1 className="text-center text-3xl font-bold">
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="mb-4 text-center text-3xl font-bold">
           Top {results.length} {qDecoded} near {locationDecoded}
         </h1>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,31 +1,36 @@
+// 這個檔案是為了生成sitemap
+// 我們需要生成一個sitemap.xml 檔案，這樣可以讓搜尋引擎知道我們的網站有哪些頁面
+// 這樣搜尋引擎就可以知道我們的網站有哪些頁面，並且可以知道哪些頁面是最重要的
+
 import { getAllTags, locations } from "@/data/restaurants";
 import { MetadataRoute } from "next";
 
+// baseUrl 是從環境變量中取得的
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const allTags = await getAllTags();
 
-  const searchLandingPages = allTags
+  const searchLandingPages: MetadataRoute.Sitemap = allTags
     .map((tag) =>
       locations.map((location) => ({
         url: `${baseUrl}/${location}/${tag}`,
         lastModified: new Date(),
-        changeFrequency: "weekly",
+        changeFrequency: "weekly" as const,
         priority: 1,
       })),
     )
-    .flat() as MetadataRoute.Sitemap;
+    .flat();
 
   return [
-    // Insert your other pages:
+    // 放入其他頁面
     {
-      url: `${baseUrl}/about`,
-      lastModified: "2024-12-31",
-      changeFrequency: "yearly",
-      priority: 0.8,
+      url: `${baseUrl}/about`, // url 是最重要的
+      // lastModified: "2024-12-31",
+      // changeFrequency: "yearly",
+      // priority: 0.8,
     },
-    // Our pSEO pages:
+    // 放入 programmatic SEO 頁面
     ...searchLandingPages,
   ];
 }
